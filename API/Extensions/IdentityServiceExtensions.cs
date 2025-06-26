c25dabd34d0efaad33cc3521af4a2f7f76c10712
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-06-26 04:55:08 +0300

    refactored the code of jwt token service into extension method

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config) 
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                 .AddJwtBearer(o =>
+                 {
+                     var tokenKey = config["TokenKey"] ?? throw new Exception("TokenKey not found");
+
+                     o.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         ValidateIssuerSigningKey = true,
+                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                         ValidateIssuer = false,
+                         ValidateAudience = false,
+                     };
+                 });
+
+            return services;
+        }
+    }
+}
