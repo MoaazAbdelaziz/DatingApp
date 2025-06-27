4e460b84748ff6b754d9d59999fa5c70206036ad
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-06-27 19:35:19 +0300

    used toastr and route and edited the design of nav and logic

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,32 +2,41 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    FormsModule,
+    BsDropdownModule,
+    TitleCasePipe,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   model: any = {};
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
 
   login() {
-    console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next: (res) => {
-        console.log(res);
+      next: (_) => {
+        this.router.navigateByUrl('/members');
       },
-      error: (err) => {
-        console.log(err);
-      },
-      complete: () => console.log('Request has completed'),
+      error: (err) => this.toastr.error(err.error),
     });
   }
 
   logout() {
+    this.model = {};
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
