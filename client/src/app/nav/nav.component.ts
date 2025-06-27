9a9de96b70ad2aac02d33e06eee1a6270f763ea3
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-06-27 04:48:17 +0300

    finished nav component logic and design

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  model: any = {};
+  accountService = inject(AccountService);
 
+  login() {
+    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => console.log('Request has completed'),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
