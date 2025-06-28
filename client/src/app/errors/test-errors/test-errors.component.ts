07fb2008dcf1d5864a70de816533488e4f8a1f59
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-06-28 03:05:35 +0300

    test validation errors

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -11,6 +11,7 @@ import { Component, inject } from '@angular/core';
 export class TestErrorsComponent {
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
+  validationErrors: string[] = [];
 
   get400Error() {
     this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
@@ -39,7 +40,10 @@ export class TestErrorsComponent {
   get400ValidiationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
       next: (res) => console.log(res),
-      error: (err) => console.log(err),
+      error: (err) => {
+        console.log(err);
+        this.validationErrors = err;
+      },
     });
   }
 }
