c598a168b6410e301d0c3bfaa3ce8dfa037b96e1
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-08 20:54:40 +0300

    edited app component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,19 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal, Signal } from '@angular/core';
+import { Nav } from '../layout/nav/nav';
+import { AccountService } from '../core/services/account-service';
+import { User } from '../types/user';
+import { Home } from '../features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   protected title = 'Dating App';
-  protected members = signal<any | []>([]);
+  protected members = signal<User[] | []>([]);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
+    this.setCurentUser();
+    this.getMembers();
+  }
+
+  setCurentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getMembers() {
+    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
       next: (response) => this.members.set(response),
       error: (error) => console.log(error),
       complete: () => console.log('Request Completed'),
