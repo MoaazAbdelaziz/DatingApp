716538e01886dbdac5abe5f32c3705ea580f537b
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-08 23:54:21 +0300

    cleaned the app ts

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,30 +8,6 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  private http = inject(HttpClient);
-  private accountService = inject(AccountService);
+export class App {
   protected router = inject(Router);
-  protected title = 'Dating App';
-  protected members = signal<User[] | []>([]);
-
-  ngOnInit(): void {
-    this.setCurentUser();
-    this.getMembers();
-  }
-
-  setCurentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  getMembers() {
-    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
-      next: (response) => this.members.set(response),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request Completed'),
-    });
-  }
 }
