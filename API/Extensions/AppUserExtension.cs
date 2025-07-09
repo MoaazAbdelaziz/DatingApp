eb57042d06ec3f8ea5e8394d592d78e1b24b5e6e
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-10 02:48:42 +0300

    sent image url to user dto

--- API/Extensions/AppUserExtension.cs
+++ API/Extensions/AppUserExtension.cs
@@ -13,6 +13,7 @@ public static class AppUserExtension
             Id = user.Id,
             DisplayName = user.DisplayName,
             Email = user.Email,
+            ImageUrl = user.ImageUrl,
             Token = service.CreateToken(user)
         };
     }
