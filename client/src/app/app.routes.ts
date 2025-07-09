2f2508daa2b285a23d38097c1688d4e1a70a0f10
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-09 11:06:33 +0300

    added routers for server error and not found

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,13 +4,17 @@ import { MemberList } from '../features/members/member-list/member-list';
 import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/gurds/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
 
 export const routes: Routes = [
   { path: '', component: Home },
   {
     path: '',
     runGuardsAndResolvers: 'always',
-    canActivate: ['authGuard'],
+    canActivate: [authGuard],
     children: [
       { path: 'members', component: MemberList },
       { path: 'members/:id', component: MemberDetailed },
@@ -18,5 +22,7 @@ export const routes: Routes = [
       { path: 'messages', component: Messages },
     ],
   },
-  { path: '**', component: Home },
+  { path: 'errors', component: TestErrors },
+  { path: 'server-error', component: ServerError },
+  { path: '**', component: NotFound },
 ];
