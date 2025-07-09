a928a2674a977ba7b2504b5486b3bb98068f0d36
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-09 11:06:49 +0300

    updated auth guard

--- client/src/core/gurds/auth-guard.ts
+++ client/src/core/gurds/auth-guard.ts
@@ -1,15 +1,17 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AccountService } from '../services/account-service';
 import { ToastService } from '../services/toast-service';
 
 export const authGuard: CanActivateFn = () => {
   const accountService = inject(AccountService);
+  const router = inject(Router);
   const toastService = inject(ToastService);
 
   if (accountService.currentUser()) return true;
   else {
     toastService.error('You shall not pass');
+    router.navigateByUrl('/');
     return false;
   }
 };
