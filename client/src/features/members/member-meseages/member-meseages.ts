dafae71f88a381ef55ad959ebf4b5514703837f4
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-12 02:25:40 +0300

    added member messages component

---
+++ client/src/features/members/member-meseages/member-meseages.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-meseages',
+  imports: [],
+  templateUrl: './member-meseages.html',
+  styleUrl: './member-meseages.css'
+})
+export class MemberMeseages {
+
+}
