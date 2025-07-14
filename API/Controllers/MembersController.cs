f91639687a019f833c9bcd6320b9e2d1b0f859af
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-14 20:22:14 +0300

    added a request to update member

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,5 @@
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -32,6 +33,29 @@ public class MembersController(IMemberRepository memberRepository) : BaseApiCont
         var photos = await memberRepository.GetPhotosForMemberAsync(id);
 
         return Ok(photos);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(UpdateMemberDto updateMemberDto)
+    {
+        var memberId = User.GetMemberId();
+
+        var member = await memberRepository.GetMemberForUpdate(memberId);
+
+        if (member is null) return BadRequest("Could not get member");
+
+        member.DisplayName = updateMemberDto.DisplayName ?? member.DisplayName;
+        member.Description = updateMemberDto.Description ?? member.Description;
+        member.City = updateMemberDto.City ?? member.City;
+        member.Country = updateMemberDto.Country ?? member.Country;
+
+        member.User.DisplayName = updateMemberDto.DisplayName ?? member.User.DisplayName;
+
+        memberRepository.Update(member);
+
+        if (await memberRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update member");
     }
 }
 
