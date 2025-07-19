13ebd6a22027da63fb6a01ab808697f280490c1c
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-19 14:55:33 +0300

    registered user activity service

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -24,6 +24,8 @@ public static class ApplicationServiceExtensions
 
         services.AddScoped<IPhotoService, PhotoService>();
 
+        services.AddScoped<LogUserActivity>();
+
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
         return services;
