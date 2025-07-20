e02d5a81d654a68652d36e116da173212de3451a
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-20 11:40:43 +0300

    updated account and init service

--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,18 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { of } from 'rxjs';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitService {
   private accoutService = inject(AccountService);
+  private likesService = inject(LikesService);
 
   init() {
     const userString = localStorage.getItem('user');
     if (!userString) return of(null);
     const user = JSON.parse(userString);
     this.accoutService.setCurrentUser(user);
+    this.likesService.getLikeIds();
 
     return of(null);
   }
