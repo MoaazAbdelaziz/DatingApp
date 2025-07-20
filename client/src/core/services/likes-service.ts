9dfd0ebb256b247b4a370cdf0205081395834894
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-20 11:39:57 +0300

    added like service

---
+++ client/src/core/services/likes-service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../../types/member';
+import { PaginationResult } from '../../types/pagination';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LikesService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+
+  likeIds = signal<string[]>([]);
+
+  toggleLike(targetMemberId: string) {
+    return this.http.post(`${this.baseUrl}likes/${targetMemberId}`, {});
+  }
+
+  getLikes(predicate: string, pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+    params = params.append('predicate', predicate);
+
+    return this.http.get<PaginationResult<Member>>(
+      `${this.baseUrl}likes?predicate=${predicate}`,
+      { params }
+    );
+  }
+
+  getLikeIds() {
+    return this.http.get<string[]>(`${this.baseUrl}likes/list`).subscribe({
+      next: (ids) => this.likeIds.set(ids),
+    });
+  }
+
+  clearLikeIds() {
+    this.likeIds.set([]);
+  }
+}
