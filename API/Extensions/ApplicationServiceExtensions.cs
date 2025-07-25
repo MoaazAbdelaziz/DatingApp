58e90c88fe3e87bd0556cb25a721629c961d9177
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-26 01:40:45 +0300

    registered presence tracker

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -33,6 +34,8 @@ public static class ApplicationServiceExtensions
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
         services.AddSignalR();
+
+        services.AddSingleton<PresenceTracker>();
 
         return services;
     }
