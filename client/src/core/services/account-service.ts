3cc305d8077ec9385029a79c05aa8f0f3759838a
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-25 21:50:21 +0300

    used presence service

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -4,6 +4,8 @@ import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LikesService } from './likes-service';
+import { PresenceService } from './presence-service';
+import { HubConnectionState } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +14,7 @@ export class AccountService {
   private http = inject(HttpClient);
   private likesService = inject(LikesService);
   private baseUrl = environment.apiUrl;
+  private presenceService = inject(PresenceService);
   currentUser = signal<User | null>(null);
 
   register(creds: RegisterCreds) {
@@ -71,6 +74,11 @@ export class AccountService {
     user.roles = this.getRolesFromToken(user);
     this.currentUser.set(user);
     this.likesService.getLikeIds();
+    if (
+      this.presenceService.hubConnection?.state !== HubConnectionState.Connected
+    ) {
+      this.presenceService.createHubConnection(user);
+    }
   }
 
   logout() {
