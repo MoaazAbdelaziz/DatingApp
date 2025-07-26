f12970a9d6631904698b9b953672b86f0d2bbbf6
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-07-26 17:34:01 +0300

    updated presence service by using online users signal to get online users that are connected to hub

--- client/src/core/services/presence-service.ts
+++ client/src/core/services/presence-service.ts
@@ -1,6 +1,5 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { ToastService } from './toast-service';
 import { User } from '../../types/user';
 import {
   HubConnection,
@@ -13,8 +12,8 @@ import {
 })
 export class PresenceService {
   private hubUrl = environment.hubUrl;
-  private toastService = inject(ToastService);
   hubConnection?: HubConnection;
+  onlineUsers = signal<string[]>([]);
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
@@ -26,13 +25,17 @@ export class PresenceService {
 
     this.hubConnection.start().catch((error) => console.log(error));
 
-    this.hubConnection.on('UserOnline', (email) =>
-      this.toastService.success(email + ' has connected')
-    );
+    this.hubConnection.on('UserOnline', (userId) => {
+      this.onlineUsers.update((users) => [...users, userId]);
+    });
 
-    this.hubConnection.on('UserOffline', (email) =>
-      this.toastService.error(email + ' has disconnected')
-    );
+    this.hubConnection.on('UserOffline', (userId) => {
+      this.onlineUsers.update((users) => users.filter((u) => u !== userId));
+    });
+
+    this.hubConnection.on('GetOnlineUsers', (userIds) => {
+      this.onlineUsers.set(userIds);
+    });
   }
 
   stopHubConnection() {
